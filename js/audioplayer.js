592a5f19a55b0b2ffc3dce352b3a8fe98fdb8224
MblJlo <40318812+MblJlo@users.noreply.github.com>
2019-06-12 21:12:27 +0300

    123

---
+++ js/audioplayer.js
@@ -0,0 +1,75 @@
+;(function( $, window, document, undefined )
+{
+	var isTouch		  = 'ontouchstart' in window,
+		eStart		  = isTouch ? 'touchstart'	: 'mousedown',
+		eMove		  = isTouch ? 'touchmove'	: 'mousemove',
+		eEnd		  = isTouch ? 'touchend'	: 'mouseup',
+		eCancel		  = isTouch ? 'touchcancel'	: 'mouseup',
+		secondsToTime = function( secs )
+		{
+			var hoursDiv = secs / 3600, hours = Math.floor( hoursDiv ), minutesDiv = secs % 3600 / 60, minutes = Math.floor( minutesDiv ), seconds = Math.ceil( secs % 3600 % 60 );
+			if( seconds > 59 ) { seconds = 0; minutes = Math.ceil( minutesDiv ); }
+			if( minutes > 59 ) { minutes = 0; hours = Math.ceil( hoursDiv ); }
+			return ( hours == 0 ? '' : hours > 0 && hours.toString().length < 2 ? '0'+hours+':' : hours+':' ) + ( minutes.toString().length < 2 ? '0'+minutes : minutes ) + ':' + ( seconds.toString().length < 2 ? '0'+seconds : seconds );
+		},
+		canPlayType	  = function( file )
+		{
+			var audioElement = document.createElement( 'audio' );
+			return !!( audioElement.canPlayType && audioElement.canPlayType( 'audio/' + file.split( '.' ).pop().toLowerCase() + ';' ).replace( /no/, '' ) );
+		};
+
+	$.fn.audioPlayer = function( params )
+	{
+		var params		= $.extend( { classPrefix: 'audioplayer', strPlay: 'Play', strPause: 'Pause', strVolume: 'Volume' }, params ),
+			cssClass	= {},
+			cssClassSub =
+			{
+				playPause:	 	'playpause',
+				playing:		'playing',
+				stopped:		'stopped',
+				time:		 	'time',
+				timeCurrent:	'time-current',
+				timeDuration: 	'time-duration',
+				bar: 			'bar',
+				barLoaded:		'bar-loaded',
+				barPlayed:		'bar-played',
+				volume:		 	'volume',
+				volumeButton: 	'volume-button',
+				volumeAdjust: 	'volume-adjust',
+				noVolume: 		'novolume',
+				muted: 			'muted',
+				mini: 			'mini'
+			};
+
+		for( var subName in cssClassSub )
+			cssClass[ subName ] = params.classPrefix + '-' + cssClassSub[ subName ];
+
+		this.each( function()
+		{
+			var thePlayer = $( '<div class="' + params.classPrefix + '">' + ( isSupport ? $( '<div>' ).append( $this.eq( 0 ).clone() ).html() : '<embed src="' + audioFile + '" width="0" height="0" volume="100" autostart="' + isAutoPlay.toString() +'" loop="' + isLoop.toString() + '" />' )),
+				theAudio  = isSupport ? thePlayer.find( 'audio' ) : thePlayer.find( 'embed' ), theAudio = theAudio.get( 0 );
+
+			thePlayer.addClass( isAutoPlay ? cssClass.playing : cssClass.stopped );
+
+			thePlayer.find( '.' + cssClass.playPause ).on( 'click', function()
+			{
+				if( thePlayer.hasClass( cssClass.playing ) )
+				{
+					$( this ).attr( 'title', params.strPlay ).find( 'a' ).html( params.strPlay );
+					thePlayer.removeClass( cssClass.playing ).addClass( cssClass.stopped );
+					isSupport ? theAudio.pause() : theAudio.Stop();
+				}
+				else
+				{
+					$( this ).attr( 'title', params.strPause ).find( 'a' ).html( params.strPause );
+					thePlayer.addClass( cssClass.playing ).removeClass( cssClass.stopped );
+					isSupport ? theAudio.play() : theAudio.Play();
+				}
+				return false;
+			});
+
+			$this.replaceWith( thePlayer );
+		});
+		return this;
+	};
+})( jQuery, window, document );
